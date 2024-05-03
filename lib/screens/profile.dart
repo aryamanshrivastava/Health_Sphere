b9899b03ced61f7306312e31dfd68c0cf865d64b
Neel-05 <avikprasad10@gmail.com>
2024-05-04 02:05:05 +0530

    Profile page signout

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,5 +1,6 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Profile extends StatefulWidget {
@@ -10,6 +11,20 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void> _logout() async {
+    try {
+      await _auth.signOut();
+      // Navigate to login screen or home screen
+      // For example:
+      // Navigator.pushReplacementNamed(context, '/login');
+    } catch (e) {
+      print('Error logging out: $e');
+    }
+  }
+
+  
   @override
   Widget build(BuildContext context) {
     double w = MediaQuery.of(context).size.width;
@@ -18,27 +33,159 @@ class _ProfileState extends State<Profile> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
-          body:SingleChildScrollView(
-            child: Center(
+            body:SingleChildScrollView(
+              padding: EdgeInsets.all(16),
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  SizedBox(height: h * 0.075),
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(100),
-                    child: Image.asset(
-                      'assets/logo_1.png'
-                    ),
+                  SizedBox(height: h * 0.01),
+                  Center(
+                    child:ClipRRect(
+                      borderRadius: BorderRadius.circular(100),
+                      child: Image.asset(
+                        'assets/logo_1.png',
+                      ),
+                    )
                   ),
                   SizedBox(height: h * 0.03),
-                  Text(
-                    "Profile"
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                        Text(
+                          "Username", 
+                          style: TextStyle(
+                            color: Color(0xffef3d49),
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "Aryaman Shrivastava", 
+                          style: TextStyle(
+                            fontSize: 21,
+                            fontWeight: FontWeight.w500
+                          ),
+                        ),
+                        SizedBox(height: h * 0.02),
+                        Text(
+                          "Email address", 
+                          style: TextStyle(
+                            color: Color(0xffef3d49),
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                        Text(
+                          "aryamans135@yahoo.com", 
+                          style: TextStyle(
+                            fontSize: 21,
+                            fontWeight: FontWeight.w500
+                          ),
+                        ),
+                        SizedBox(height: h * 0.02),
+                        Text(
+                          "Phone Number", 
+                          style: TextStyle(
+                            color: Color(0xffef3d49),
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                        Text(
+                          "+91-7067250520", 
+                          style: TextStyle(
+                            fontSize: 21,
+                            fontWeight: FontWeight.w500
+                          ),
+                        ),
+                        SizedBox(height: h * 0.02),
+                        Text(
+                          "Age", 
+                          style: TextStyle(
+                            color: Color(0xffef3d49),
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                        Text(
+                          "22", 
+                          style: TextStyle(
+                            fontSize: 21,
+                            fontWeight: FontWeight.w500
+                          ),
+                        ),
+                        SizedBox(height: h * 0.02),
+                        Text(
+                          "Gender", 
+                          style: TextStyle(
+                            color: Color(0xffef3d49),
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold
+                          ),
+                        ),
+                        Text(
+                          "Male", 
+                          style: TextStyle(
+                            fontSize: 21,
+                            fontWeight: FontWeight.w500
+                          ),
+                        ),
+                    ],
+                  ),
+                  SizedBox(height: h * 0.04),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton.icon(
+                        onPressed: () {
+
+                        },
+                        icon: Icon(Icons.edit),
+                        label: Text(
+                          "Edit",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                          ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          iconColor: Colors.white, // Background color
+                          backgroundColor: Colors.black, // Text color
+                          minimumSize: Size(w * 0.40, h * 0.06),
+                          shape: RoundedRectangleBorder( // Button border radius
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          elevation: 3,
+                        ),
+                      ),
+                      ElevatedButton.icon(
+                        onPressed: () {
+                          _logout();
+                        },
+                        icon: Icon(Icons.logout_rounded),
+                        label: Text(
+                          "Logout",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 15,
+                          ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          iconColor: Colors.white, // Background color
+                          backgroundColor: Colors.black, // Text color
+                          minimumSize: Size(w * 0.40, h * 0.06),
+                          shape: RoundedRectangleBorder( // Button border radius
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          elevation: 3,
+                        ),
+                      ),
+                    ],
                   ),
                 ],
             ),
           )
         )
-      )
-    ); 
+      );
   }
 }
