59318e81b89e33c7d4ad9f907f559c66f9e2c6aa
Neel-05 <avikprasad10@gmail.com>
2024-05-04 12:26:15 +0530

    Health tips done

--- lib/data/healthtips.dart
+++ lib/data/healthtips.dart
@@ -11,63 +11,69 @@ class HealthTips {
 }
 final List<HealthTips> healthTips = [
   HealthTips(
-    title: 'Stay Active',
+    title: 'Stay active',
     description:
         'Incorporate physical activity into your daily routine to improve cardiovascular health and boost mood.',
     imageUrl: 'assets/stay_active.png',
   ),
   HealthTips(
-    title: 'Eat a Balanced Diet',
+    title: 'Eat a balanced diet',
     description:
         'Consume a variety of fruits, vegetables, lean proteins, and whole grains for essential nutrients.',
     imageUrl: 'assets/1.png',
   ),
   HealthTips(
-    title: 'Stay Hydrated',
+    title: 'Stay hydrated',
     description:
         'Drink plenty of water throughout the day to support digestion, circulation, and overall health.',
     imageUrl: 'assets/stay_hydrated.png',
   ),
   HealthTips(
-    title: 'Get Sufficient Sleep',
+    title: 'Get sufficient sleep',
     description:
         'Aim for 7-9 hours of quality sleep each night to rejuvenate your body and mind.',
     imageUrl: 'assets/sleep.png',
   ),
   HealthTips(
-    title: 'Manage Stress',
+    title: 'manage stress',
     description:
         'Practice stress-reducing techniques such as deep breathing or meditation to alleviate tension.',
     imageUrl: 'assets/manage_stress.png',
   ),
   HealthTips(
-    title: 'Limit Sugary and Processed Foods',
+    title: 'Limit sugar',
     description:
-        'Minimize intake of sugary drinks and processed snacks to prevent weight gain and energy crashes.',
-    imageUrl: 'assets/limit_sugar.jpg',
+        'Minimize intake of sugary drinks.',
+    imageUrl: 'assets/sugar-free.png',
+  ),
+   HealthTips(
+    title: 'Avoid processed foods',
+    description:
+        'Processed snacks are causes for weight gain and energy crashes.',
+    imageUrl: 'assets/smart-factory.png',
   ),
   HealthTips(
     title: 'Prioritize Mental Health',
     description:
         'Take time for self-care activities and seek support from loved ones or professionals when needed.',
-    imageUrl: 'assets/prioritize_mental_health.jpg',
+    imageUrl: 'assets/mental-health.png',
   ),
   HealthTips(
     title: 'Practice Good Hygiene',
     description:
         'Maintain proper hygiene habits such as regular handwashing and dental care to prevent infections.',
-    imageUrl: 'assets/good_hygiene.jpg',
+    imageUrl: 'assets/hygiene.png',
   ),
   HealthTips(
     title: 'Stay Connected',
     description:
         'Foster meaningful relationships to cultivate a support network that enhances emotional resilience.',
-    imageUrl: 'assets/stay_connected.jpg',
+    imageUrl: 'assets/network.png',
   ),
   HealthTips(
     title: 'Listen to Your Body',
     description:
-        'Pay attention to your body\'s signals and respond appropriately by resting when needed and seeking medical attention for any concerning symptoms.',
-    imageUrl: 'assets/listen_to_body.jpg',
+        'Pay attention to your body\'s signals and respond appropriately by seeking medical attention.',
+    imageUrl: 'assets/attention.png',
   ),
 ];
--- lib/screens/tips.dart
+++ lib/screens/tips.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import '../data/healthtips.dart';
 
 class Tips extends StatefulWidget {
   const Tips({super.key});
@@ -10,13 +13,93 @@ class Tips extends StatefulWidget {
 class _TipsState extends State<Tips> {
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    double w = MediaQuery.of(context).size.width;
+    double h = MediaQuery.of(context).size.height;
+    
+    return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
-        body: Text(
-          "Health tips"
+        body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              SizedBox(height: h * 0.03),
+              SizedBox(
+                
+                width: w,
+                child: Text(
+                  "These are some of the simple tips stay healthy.", 
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 22
+                  ),
+                ),
+              ),
+              SizedBox(height: h * 0.02),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: healthTips.length,
+                  scrollDirection: Axis.vertical,
+                  itemBuilder: (BuildContext context, int index) {
+                    HealthTips tip = healthTips[index];
+                    return Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black, width: 1.0),
+                            color: Color(0xffffffff),
+                            borderRadius: BorderRadius.circular(h * 0.02),
+                          ),
+                          margin: EdgeInsets.only(bottom: h * 0.015),
+                          padding: EdgeInsets.all(10),
+                          child: Row(
+                            children: [
+                              Container(
+                                width: w * 0.2,
+                                height: h * 0.1,
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    image: AssetImage(tip.imageUrl),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(width: w * 0.05),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    tip.title,
+                                    style: TextStyle(
+                                      color: Color(0xff000000),
+                                      fontSize: h * 0.02,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(height: h * 0.005),
+                                  SizedBox(
+                                    width: w * 0.55,
+                                    child: Text(
+                                      tip.description,
+                                      maxLines: 3,
+                                      overflow: TextOverflow.ellipsis,
+                                      style: TextStyle(
+                                        color: Color(0xff000000),
+                                        fontSize: h * 0.015,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          )
+                    );
+              }
+          )
         ),
+
+            ],
+          ),
       )
-    ); 
+    ));
   }
 }
